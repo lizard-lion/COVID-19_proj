48731b8359ff4eba7452f23ad37bab628a68e787
lizard-lion <kt9393@likelion.org>
2020-03-03 20:30:43 +0900

    page layout complete

--- virus/urls.py
+++ virus/urls.py
@@ -4,4 +4,8 @@ from virus import views
 
 urlpatterns = [
     path('', views.main, name="main"),
+    path('about/', views.about, name="about"),
+    path('hospital/', views.hospital, name="hospital"),
+    path('map/', views.map, name="map"),
+    path('virus/', views.virus, name="virus"),
 ]
--- virus/views.py
+++ virus/views.py
@@ -2,3 +2,27 @@ from django.shortcuts import render
 
 def main(request):
     return render(request, 'main.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def hospital(request):
+    return render(request, 'hospital.html')
+
+def map(request):
+
+    testdata = {
+        "name" : "1번 환자",
+        "paths" : ['37.25400, 126.59302', '37.23374, 126.55239', '37.012345, 127.259493'],
+        "mapid" : 1
+    }
+    data = [
+        testdata
+    ]
+
+    return render(request, 'map.html', {
+        'data' : data,
+    })
+
+def virus(request):
+    return render(request, 'virus.html')
