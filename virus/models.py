019b7a5fcd697029244cbb2e2d76824e5031b4e9
lizard-lion <kt9393@likelion.org>
2020-03-04 22:49:55 +0900

    Add comments

--- virus/models.py
+++ virus/models.py
@@ -11,4 +11,19 @@ class Posting(models.Model):
         blank = True,
     )
     updated_at = models.DateTimeField(blank=True, null=True)
+
+class Comments(models.Model):
+    post_num = models.IntegerField(
+        null= True,
+        blank = True,
+    )
+    user = models.CharField(
+        max_length = 20,
+        default = 'anonymous'
+    )
+    content = models.TextField(
+        null= True,
+        blank = True,
+    )
+    updated_at = models.DateTimeField(blank=True, null=True)
     
