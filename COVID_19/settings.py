f689eed2dd556eaa1e585444a7d2613dfaa0faab
lizard-lion <kt9393@likelion.org>
2020-03-04 21:06:26 +0900

    allow all host

--- COVID_19/settings.py
+++ COVID_19/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gi9(w%w1ve#d+$xbkl%pghgczgac4-dhija3-0oljelq7%suc7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
